3c02383df6fef929431b3ed7174224326f0857e8
mudassir060 <mudassirmukhtar4@gmail.com>
2023-02-03 01:43:59 +0500

    Notification

--- lib/notification.dart
+++ lib/notification.dart
@@ -11,35 +11,32 @@ class _notificationState extends State<notification> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: Container(),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                (Color.fromARGB(255, 99, 4, 4)),
+                (Color.fromARGB(255, 99, 4, 4)),
+                (Color.fromARGB(255, 223, 24, 17))
+              ],
+            ),
+          ),
+        ),
+        title: Text(
+          "NOTIFICATIONS",
+          style: const TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Container(
-                  height: MediaQuery.of(context).size.height * 0.1,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        (Color.fromARGB(255, 99, 4, 4)),
-                        (Color.fromARGB(255, 223, 24, 17))
-                      ],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "NOTIFICATIONS",
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                  )),
-            ),
-          ],
+          children: [],
         ),
       ),
     );
