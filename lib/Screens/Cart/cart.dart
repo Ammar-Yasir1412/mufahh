6c644910c7b3662c39ba71804bf8f370a5eceb11
mudassir060 <mudassirmukhtar4@gmail.com>
2023-02-06 11:22:37 +0500

    Add to card

--- lib/Screens/Cart/cart.dart
+++ lib/Screens/Cart/cart.dart
@@ -1,10 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:dropdown_search/dropdown_search.dart';
-import 'package:mufahh/Screens/Categories/Categories.dart';
-
-import '../../constants/data.dart';
 import 'Itemdetails.dart';
 
 class Cart extends StatefulWidget {
@@ -17,25 +12,14 @@ class Cart extends StatefulWidget {
 }
 
 class _CartState extends State<Cart> {
-  List images = [
-    {"name": 'Phone ', "picture": 'assets/iphone.jpg'},
-    {"name": 'Car ', "picture": 'assets/Civic.jpg'},
-    {"name": 'Furniture ', "picture": 'assets/sofa.jpg'},
-    {"name": 'Home Appliances ', "picture": 'assets/Refri.jpg'}
-  ];
-
-
-  final Stream<QuerySnapshot> _itemStream = FirebaseFirestore.instance
-      .collection('products')
-  // .orderBy('Date', descending: true)
-      .where('bidClose', isEqualTo: false)
-  // .limitToLast(2)l
-      .snapshots();
-  final Stream<QuerySnapshot> _sliderStream =
-  FirebaseFirestore.instance.collection('Slider').snapshots();
-
   @override
   Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> _itemStream = FirebaseFirestore.instance
+        .collection('products')
+    // .orderBy('Date', descending: true)
+        .where('winner', isEqualTo: widget.UserData["UID"])
+    // .limitToLast(2)
+        .snapshots();
     return Scaffold(
       backgroundColor: Color.fromARGB(226, 75, 41, 41),
       body: SingleChildScrollView(
@@ -69,7 +53,7 @@ class _CartState extends State<Cart> {
                       );
                     }
                     if (snapshot.data?.size == 0) {
-                      return Center(child: const Text("No data found"));
+                      return const Center(child: Text("No data found"));
                     }
                     return GridView.extent(
                       primary: false,
@@ -91,80 +75,6 @@ class _CartState extends State<Cart> {
                     );
                   },
                 ),
-                // SizedBox(height: 10),
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                //   children: [
-                //     Container(
-                //       margin: const EdgeInsets.only(top: 20, left: 10, right: 10),
-                //       alignment: Alignment.center,
-                //       child: const Text(
-                //         "Categories ",
-                //         style: TextStyle(color: Colors.yellow),
-                //       ),
-                //     ),
-                //     GestureDetector(
-                //       onTap: (() => {
-                //             Navigator.push(
-                //               context,
-                //               MaterialPageRoute(builder: (context) => Cate()),
-                //             )
-                //           }),
-                //       child: Container(
-                //         margin: const EdgeInsets.only(top: 20, left: 20, right: 10),
-                //         alignment: Alignment.center,
-                //         child: const Text(
-                //           "See more >>",
-                //           style: TextStyle(color: Colors.yellow),
-                //         ),
-                //       ),
-                //     ),
-                //   ],
-                // ),
-                // const SizedBox(height: 15),
-                // GridView.builder(
-                //   shrinkWrap: true,
-                //   itemCount: images.length,
-                //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                //       crossAxisCount: 2,
-                //       crossAxisSpacing: 4.0,
-                //       mainAxisSpacing: 4.0),
-                //   itemBuilder: (BuildContext context, int index) {
-                //     return Card(
-                //         child: GestureDetector(
-                //       onTap: () {
-                //         Navigator.push(context,
-                //             MaterialPageRoute(builder: (context) => Itm()));
-                //       },
-                //       child: Container(
-                //           decoration: BoxDecoration(
-                //             image: DecorationImage(
-                //               image: AssetImage(images[index]['picture']),
-                //               fit: BoxFit.fill,
-                //             ),
-                //             shape: BoxShape.rectangle,
-                //           ),
-                //           child: Column(
-                //             mainAxisAlignment: MainAxisAlignment.end,
-                //             children: [
-                //               Container(
-                //                 color: Colors.black.withOpacity(0.5),
-                //                 height: 25,
-                //                 child: Row(
-                //                   mainAxisAlignment: MainAxisAlignment.center,
-                //                   children: [
-                //                     Text(
-                //                       images[index]['name'],
-                //                       style: const TextStyle(color: Colors.white),
-                //                     ),
-                //                   ],
-                //                 ),
-                //               ),
-                //             ],
-                //           )),
-                //     ));
-                //   },
-                // ),
               ]),
         ),
       ),
