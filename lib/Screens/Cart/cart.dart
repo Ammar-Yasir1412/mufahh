1998ab54d502dba08dc507fbcefc80561acc8f11
mudassir060 <mudassirmukhtar4@gmail.com>
2023-02-06 11:34:05 +0500

    Cart Card

--- lib/Screens/Cart/cart.dart
+++ lib/Screens/Cart/cart.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'Itemdetails.dart';
+import 'cart_card.dart';
 
 class Cart extends StatefulWidget {
   final Map UserData;
@@ -16,66 +16,73 @@ class _CartState extends State<Cart> {
   Widget build(BuildContext context) {
     final Stream<QuerySnapshot> _itemStream = FirebaseFirestore.instance
         .collection('products')
-    // .orderBy('Date', descending: true)
+        // .orderBy('Date', descending: true)
         .where('winner', isEqualTo: widget.UserData["UID"])
-    // .limitToLast(2)
+        // .limitToLast(2)
         .snapshots();
     return Scaffold(
-      backgroundColor: Color.fromARGB(226, 75, 41, 41),
+      appBar: AppBar(
+        leading: Container(),
+        centerTitle: true,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                (Color.fromARGB(255, 99, 4, 4)),
+                (Color.fromARGB(255, 99, 4, 4)),
+                (Color.fromARGB(255, 223, 24, 17))
+              ],
+            ),
+          ),
+        ),
+        title: const Text(
+          "Cart",
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
       body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(12.0),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                const Text(
-                  "Neared By",
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                StreamBuilder<QuerySnapshot>(
-                  stream: _itemStream,
-                  builder: (BuildContext context,
-                      AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.hasError) {
-                      return Column(
-                        children: const [
-                          Text('Something went wrong'),
-                        ],
-                      );
-                    }
+        child: StreamBuilder<QuerySnapshot>(
+          stream: _itemStream,
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Column(
+                children: const [
+                  Text('Something went wrong'),
+                ],
+              );
+            }
 
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    if (snapshot.data?.size == 0) {
-                      return const Center(child: Text("No data found"));
-                    }
-                    return GridView.extent(
-                      primary: false,
-                      padding: const EdgeInsets.all(16),
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 10,
-                      childAspectRatio: (1 / 1.18),
-                      shrinkWrap: true,
-                      maxCrossAxisExtent: 200.0,
-                      children:
-                      snapshot.data!.docs.map((DocumentSnapshot document) {
-                        Map<String, dynamic> data =
-                        document.data()! as Map<String, dynamic>;
-                        return         ItemDetails(
-                          data: data,
-                          UserData: widget.UserData,
-                        );
-                      }).toList(),
-                    );
-                  },
-                ),
-              ]),
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.data?.size == 0) {
+              return const Center(child: Text("No data found"));
+            }
+            return GridView.extent(
+              primary: false,
+              padding: const EdgeInsets.all(16),
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: (1 / 1.18),
+              shrinkWrap: true,
+              maxCrossAxisExtent: 200.0,
+              children:
+                  snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+                return cart_card(
+                  data: data,
+                  UserData: widget.UserData,
+                );
+              }).toList(),
+            );
+          },
         ),
       ),
     );
---
+++ lib/Screens/Cart/cart_card.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import '../../Functions/addToCart.dart';
+import '../../Widgets/drop_button.dart';
+import '../Dashboard/item_detail.dart';
+
+class cart_card extends StatefulWidget {
+  final Map data;
+  final Map UserData;
+
+  const cart_card({
+    Key? key,
+    required this.data,
+    required this.UserData,
+  }) : super(key: key);
+
+  @override
+  State<cart_card> createState() => _cart_cardState();
+}
+
+class _cart_cardState extends State<cart_card> {
+  bool _disable = false;
+  // bool like = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => item_detail(
+                      data: widget.data,
+                      UserData: widget.UserData,
+                    )));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(10),
+          ),
+          color: Colors.grey[200],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                          width: 190.00,
+                          height: 120.00,
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10)),
+                            image: _disable
+                                ? const DecorationImage(
+                                    image: AssetImage("logo"),
+                                    fit: BoxFit.fill,
+                                  )
+                                : DecorationImage(
+                                    image:
+                                        NetworkImage('${widget.data["url"]}'),
+                                    fit: BoxFit.fill,
+                                  ),
+                          )),
+
+                    ],
+                  ),
+                  SizedBox(
+                    width: 200,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          '${widget.data["title"]}',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        // Text(
+                        //   '${widget.data["category"]}',                          overflow: TextOverflow.ellipsis,
+                        //
+                        //   style: TextStyle(color: Colors.black),
+                        // ),
+                        // Text(
+                        //   "${widget.data["description"]}",
+                        //   overflow: TextOverflow.ellipsis,
+                        //
+                        //   style: TextStyle(color: Colors.black),
+                        // ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              widget.data["UID"] == widget.UserData["UID"]
+                  ? Positioned(
+                      right: -7,
+                      bottom: -5,
+                      child: drop_button(
+                          Coll: "products",
+                          Doc: widget.data["Key"],
+                          disable: !_disable))
+                  : Container()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
