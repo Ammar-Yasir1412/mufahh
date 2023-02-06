87e1f95d8522c290cd39dd9976d83ef57ea28468
mudassir060 <mudassirmukhtar4@gmail.com>
2023-02-06 11:56:59 +0500

    cart

--- lib/Screens/Cart/cart_detail.dart
+++ lib/Screens/Cart/cart_detail.dart
@@ -1,9 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import '../../Functions/toast.dart';
-import '../../Widgets/myLargeButton.dart';
-import '../../Widgets/myTextField.dart';
+import 'package:mufahh/constants/style.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class cart_detail extends StatefulWidget {
   final Map data;
@@ -22,107 +20,36 @@ class _cart_detailState extends State<cart_detail> {
 
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  addBid() {
-    showDialog(
-      context: context,
-      builder: (ctx) => AlertDialog(
-        title: const Text("Add Your Bid"),
-        content: myTextField(
-          context,
-          "Enter your Amount",
-          Icons.person,
-          amountCTRL,
-        ),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              bidNow();
-            },
-            child: Container(
-              padding: const EdgeInsets.all(14),
-              child: const Text("Confirm"),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  bidNow() async {
-    setState(() {
-      looding = true;
-    });
-    var amount = amountCTRL.text;
-
-    try {
-      if (amount != '') {
-        DateTime now = DateTime.now();
-        String formattedDate = DateFormat('EEE d MMM').format(now);
-        var data = widget.data['Bid'] ?? [];
-        data.add({
-          "UID": widget.UserData["UID"],
-          "amount": amount,
-          "address": widget.UserData["address"],
-          "username": widget.UserData["username"],
-          "email": widget.UserData["email"],
-          "PhoneNo": widget.UserData["PhoneNo"],
-          "JoinDate": formattedDate,
-        });
-        await firestore
-            .collection("products")
-            .doc(widget.data["Key"])
-            .update({"Bid": data});
-        toast("Bid Uploaded");
-      } else {
-        toast("Please fill all text field");
-      }
-    } catch (e) {
-      print(e);
-      toast(e.toString());
-    }
-    setState(() {
-      looding = false;
-    });
-  }
-
-  closeBid() async {
-    setState(() {
-      looding = true;
-    });
-    var bidData = widget.data['Bid'];
-    var bidWiner = bidData[0];
-    try {
-      var largervalue = 0;
-      for (int i = 0; i < bidData.length; i++) {
-        if (int.parse(bidData[i]["amount"]) > largervalue) {
-          largervalue = int.parse(bidData[i]["amount"]);
-          bidWiner = bidData[i];
-        }
-      }
-      DateTime now = DateTime.now();
-      String formattedDate = DateFormat('EEE d MMM').format(now);
-      await firestore
-          .collection("products")
-          .doc(widget.data["Key"])
-          .update({"bidClose": true, "winner": bidWiner["UID"]});
-      await firestore.collection("notification").doc().set({
-        "title":
-        "Congratulation ${bidWiner["username"]} you are won the ${widget.data["title"]}. Contact the owner and get the product.",
-        "UID": bidWiner["UID"],
-        "JoinDate": formattedDate,
-      });
-      toast("Bid Complete");
-      Navigator.pop(context);
-    } catch (e) {
-      toast(e.toString());
-    }
-    setState(() {
-      looding = false;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    var number = widget.data["PhoneNo"];
+    var text =
+        "Dear ${widget.data["stallname"]}, we are grateful for your support in feeding those in need. Could we please request a donation of food to help us continue our mission of fighting hunger in the community? Thank you for your generosity";
+    // " hi! ${widget.stalldata["stallname"]} I want to buy $amount ${widget.itemdata["foodname"]} in $totelprice Rupees. in which time you will deliver";
+    Future<void> whatsappsms() async {
+      final Uri _url = Uri.parse("whatsapp://send?phone=$number&text=$text");
+      if (await canLaunchUrl(_url)) {
+        await launchUrl(_url);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("Whatsapp not installed")));
+      }
+    }
+
+    Future<void> simsms() async {
+      final Uri _url = Uri.parse("sms:$number?body=$text");
+      if (await canLaunchUrl(_url)) {
+        await launchUrl(_url);
+      }
+    }
+
+    Future<void> call() async {
+      final Uri _url = Uri.parse("tel:$number");
+      if (await canLaunchUrl(_url)) {
+        await launchUrl(_url);
+      }
+    }
+
     var vwidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
@@ -203,39 +130,93 @@ class _cart_detailState extends State<cart_detail> {
                   _spacer(),
                   widget.data['Bid'] != null
                       ? Column(
-                    children: [
-                      Center(
-                        child: Text(
-                          "Biddings ",
-                          style: TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black),
-                        ),
-                      ),
-                      ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: widget.data['Bid'].length,
-                          itemBuilder: (BuildContext context, int index) {
-                            var _data = widget.data['Bid'][index];
-                            return Row(
-                              mainAxisAlignment:
-                              MainAxisAlignment.spaceBetween,
-                              children: [
-                                _text(_data['username']),
-                                _text(_data['amount']),
-                              ],
-                            );
-                          }),
-                    ],
-                  )
+                          children: [
+                            Center(
+                              child: Text(
+                                "Biddings ",
+                                style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ),
+                            ListView.builder(
+                                shrinkWrap: true,
+                                itemCount: widget.data['Bid'].length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  var _data = widget.data['Bid'][index];
+                                  return Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      _text(_data['username']),
+                                      _text(_data['amount']),
+                                    ],
+                                  );
+                                }),
+                          ],
+                        )
                       : Container(),
                   _spacer(),
                   Center(
-                      child: widget.data["UID"] == widget.UserData["UID"]
-                          ? myLargeButton(
-                          context, "Close Bid", closeBid, looding)
-                          : myLargeButton(context, "Bid Now", addBid, looding)),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            (Color.fromARGB(255, 99, 4, 4)),
+                            (Color.fromARGB(255, 223, 24, 17))
+                          ],
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.only(
+                          top: 6.0,
+                          right: 22.0,
+                          left: 22.0,
+                          bottom: 6.0,
+                        ),
+                        child: Text(
+                          "Contact with",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 22,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  _spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      IconButton(
+                          onPressed: whatsappsms,
+                          icon: Icon(
+                            color: Colors.green,
+                            Icons.whatsapp,
+                            size: 40,
+                          )),
+                      IconButton(
+                          onPressed: call,
+                          color: Colors.blue,
+                          icon: Icon(
+                            Icons.phone,
+                            size: 40,
+                          )),
+                      IconButton(
+                          onPressed: simsms,
+                          icon: Icon(
+                            color: appBarColor,
+                            Icons.message,
+                            size: 40,
+                          )),
+                    ],
+                  )
                 ],
               ),
             ),
